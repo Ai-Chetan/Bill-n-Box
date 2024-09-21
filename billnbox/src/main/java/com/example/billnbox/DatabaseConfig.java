f19f68b2bce03b593f18779a476741ebe7bcbf12
Chetan Chaudhari <chetankc2005@gmail.com>
2024-09-21 18:14:35 +0530

    Edit Employee Feature Added

--- billnbox/src/main/java/com/example/billnbox/DatabaseConfig.java
+++ billnbox/src/main/java/com/example/billnbox/DatabaseConfig.java
@@ -2,9 +2,9 @@ package com.example.billnbox;
 
 public class DatabaseConfig {
     // Add Credentials here!
-    private static final String URL = "jdbc:mysql://localhost:3306/BillNBoxDB";
-    private static final String USER = "root";
-    private static final String PASSWORD = "yash22@Ty23S5AfB";
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
 
     public static String getUrl() {
         return URL;
