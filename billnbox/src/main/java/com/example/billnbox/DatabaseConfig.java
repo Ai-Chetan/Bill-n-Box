cbc2d635a332ee6f0ee0110260340e7e51fa4fbc
Chetan Chaudhari <chetankc2005@gmail.com>
2024-09-21 13:10:12 +0530

    Blank DB Test

--- billnbox/src/main/java/com/example/billnbox/DatabaseConfig.java
+++ billnbox/src/main/java/com/example/billnbox/DatabaseConfig.java
@@ -1,6 +1,7 @@
 package com.example.billnbox;
 
 public class DatabaseConfig {
+    // Add Credentials here!
     private static final String URL = "jdbc:mysql://localhost:3306/BillNBoxDB";
     private static final String USER = "root";
     private static final String PASSWORD = "yash22@Ty23S5AfB";
